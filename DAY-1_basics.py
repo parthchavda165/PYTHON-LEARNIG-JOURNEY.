2cec90f77d1f47786b65c7ae327d4b7ed3d10dae
parthchavda165 <parthchavda165@gmail.com>
2025-08-24 20:03:13 +0530

    Create DAY-1_basics.py
    Add Day-1 basic Python programs

---
+++ DAY-1_basics.py
@@ -0,0 +1,17 @@
+# Day 1 - Python Basics
+
+# Print Hello World
+print("Hello, World!")
+
+# Variables
+x = 10
+y = 20
+print("Sum is:", x + y)
+
+# Taking user input
+name = input("Enter your name: ")
+print("Welcome", name)
+
+# Age calculation
+age = int(input("Enter your age: "))
+print(f"After 5 years, you will be {age + 5} years old")
